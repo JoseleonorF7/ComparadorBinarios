b469d25a00126704df29760993ed68354afe7ae9
Jose F7 <recuerdaporquelohaces@gmail.com>
2024-09-28 21:23:24 -0500

    JAJJAJAJA

--- src/main/java/com/example/COMPARAR/BINARIOS/config.java
+++ src/main/java/com/example/COMPARAR/BINARIOS/config.java
@@ -1,9 +1,11 @@
 package com.example.COMPARAR.BINARIOS;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@EnableWebMvc
 public class config implements WebMvcConfigurer {
 
     @Override
